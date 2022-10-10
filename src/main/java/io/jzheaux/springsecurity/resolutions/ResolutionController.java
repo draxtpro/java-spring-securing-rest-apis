ce9c235d975132d6f9c9276c1928df05e5111188
Süleyman Bilgi <sueleyman.bilgi@andrena.de>
2022-10-10 15:20:37 +0200

    annotate rest methods with @PreAuthorize

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -1,15 +1,9 @@
 package io.jzheaux.springsecurity.resolutions;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -22,22 +16,26 @@ public class ResolutionController {
 	}
 
 	@GetMapping("/resolutions")
+	@PreAuthorize("hasAuthority('resolution:read')")
 	public Iterable<Resolution> read() {
 		return this.resolutions.findAll();
 	}
 
 	@GetMapping("/resolution/{id}")
+	@PreAuthorize("hasAuthority('resolution:read')")
 	public Optional<Resolution> read(@PathVariable("id") UUID id) {
 		return this.resolutions.findById(id);
 	}
 
 	@PostMapping("/resolution")
+	@PreAuthorize("hasAuthority('resolution:write')")
 	public Resolution make(@CurrentUsername String owner, @RequestBody String text) {
 		Resolution resolution = new Resolution(text, owner);
 		return this.resolutions.save(resolution);
 	}
 
 	@PutMapping(path="/resolution/{id}/revise")
+	@PreAuthorize("hasAuthority('resolution:write')")
 	@Transactional
 	public Optional<Resolution> revise(@PathVariable("id") UUID id, @RequestBody String text) {
 		this.resolutions.revise(id, text);
@@ -45,6 +43,7 @@ public class ResolutionController {
 	}
 
 	@PutMapping("/resolution/{id}/complete")
+	@PreAuthorize("hasAuthority('resolution:write')")
 	@Transactional
 	public Optional<Resolution> complete(@PathVariable("id") UUID id) {
 		this.resolutions.complete(id);

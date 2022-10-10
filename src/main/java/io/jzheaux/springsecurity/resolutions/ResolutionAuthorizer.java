d20afddcf961e6f715a4e7ca1131b82b1068438b
Süleyman Bilgi <sueleyman.bilgi@andrena.de>
2022-10-10 18:59:39 +0200

    ex 2.7: extract authorization expression into bean

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionAuthorizer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionAuthorizer.java
@@ -1,4 +1,35 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import org.springframework.security.access.expression.method.MethodSecurityExpressionOperations;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("post")
 public class ResolutionAuthorizer {
+
+//  replaces @PostFilter("filterObject.owner == authentication.name  || hasRole('ADMIN')") in ResolutionController
+    public boolean filter(MethodSecurityExpressionOperations operations){
+        if (operations.hasRole("ADMIN")) {
+            return true;
+        }
+
+        String name = operations.getAuthentication().getName();
+        Resolution resolution = (Resolution) operations.getReturnObject();
+        return resolution.getOwner().equals(name);
+
+    }
+
+//  replaces @PostAuthorize("returnObject.orElse(null)?.owner == authentication.name  || hasRole('ADMIN')") in ResolutionController
+    public boolean authorize(MethodSecurityExpressionOperations operations) {
+        if (operations.hasRole("ADMIN")) {
+            return true;
+        }
+
+        String name = operations.getAuthentication().getName();
+        Optional<Resolution> optResolution = (Optional<Resolution>) operations.getReturnObject();
+        return optResolution.map(Resolution::getOwner)
+                .filter(owner -> owner.equals(name)).isPresent();
+
+    }
 }
